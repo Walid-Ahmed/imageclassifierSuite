763d8d7c7bd46867ff4663d32b28a9fa43b5347b
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-10-14 09:50:36 -0400

    adding images

--- trainStandardDatasetMulticlass.py
+++ trainStandardDatasetMulticlass.py
@@ -34,6 +34,7 @@ from keras.datasets import mnist
 from sklearn.metrics import confusion_matrix
 
 import argparse
+from util import helper
 
 
 
@@ -44,65 +45,7 @@ import os
 from imutils import build_montages
 
 
-def plot_confusion_matrix(y_true, y_pred, classes,dataset,
-                          normalize=False,
-                          title=None,
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
 
-    # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-    # Only use the labels that appear in the data
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=labels, yticklabels=labels,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    fileToSaveConfusionMatrix=os.path.join("Results",dataset+'_Confusion Matrix.png')
-    plt.savefig(fileToSaveConfusionMatrix)
-    print("[INFO] Confusion matrix   saved to {}".format(fileNameToSaveLabels))
-
-    plt.show()
-    plt.savefig('books_read.png')
-
-
-    return ax
 
 
 if __name__ == '__main__':
